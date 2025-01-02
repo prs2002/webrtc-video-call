02ff002b4eeff171417e15ac5a0005fbf69db7bc
prs2002 <praneetsuyal+github@gmail.com>
2025-01-03 01:15:01 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import http from "http"
+import { dirname, join } from "path";
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.get("/",(req,res)=> {
+    console.log(__dirname);
+    res.sendFile(join(__dirname,'app/index.html'));
+})
+
+io.on('connection', (socket) => {
+    console.log(`a user connected to socket server with id ${socket.id}`);
+  });
+
+app.listen(9000, ()=> {
+    console.log(`server listning on port 9000`);
+})
